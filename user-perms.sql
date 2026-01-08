2c0b37c4e24f9ebfe65aa9c33fef017b7f89a550
Jake Lim <70430405+jakelime@users.noreply.github.com>
2026-01-08 16:59:08 +0800

    Amend user permissions in SQL script

--- user-perms.sql
+++ user-perms.sql
@@ -1,29 +1,34 @@
 /*
--- 1. Grant Schema Usage (Required for everyone)
+-- 1. Grant Schema Usage & Create (AMENDED)
+-- USAGE: Allows access to look up objects in the schema.
+-- CREATE: Allows creating new objects (tables, views, etc.) in the schema.
 */
-GRANT USAGE ON SCHEMA public TO matrappuser;
+GRANT USAGE, CREATE ON SCHEMA public TO matrappuser;
+
+-- Reader still only needs USAGE
 GRANT USAGE ON SCHEMA public TO matrreader;
- 
- /*
+
+/*
 -- 2. Grant Table Permissions for EXISTING tables (if any)
 -- App User: Read & Write
 */
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO matrappuser;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO matrappuser; -- needed for ID auto-increments
- 
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO matrappuser;
+
 -- Reader: Read Only
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO matrreader;
- 
+
 /*
--- 3. Configure Default Privileges (CRITICAL STEP)
--- This ensures these users automatically get permissions on NEW tables you create in the future.
--- For App User (Read/Write on future tables)
+-- 3. Configure Default Privileges
+-- Note: This applies to tables created by the USER RUNNING THIS SCRIPT (usually the admin/superuser).
 */
+-- For App User (Read/Write on future tables created by Admin)
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO matrappuser;
+
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT USAGE, SELECT ON SEQUENCES TO matrappuser;
- 
--- For Reader (Read-only on future tables)
+
+-- For Reader (Read-only on future tables created by Admin)
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT ON TABLES TO matrreader;
